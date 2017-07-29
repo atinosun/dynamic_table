227a00807a2cbe3342125359e330ce681d6241f6
atinosun <atinosun@yeah.net>
2017-07-30 00:10:23 +0800

    done.

--- routes/web/users.js
+++ routes/web/users.js
@@ -7,40 +7,28 @@
 var express = require('express');
 
 var router = express.Router();
-
+var request = require("request");
+var constantHelper = require("../../helpers/constanthelper");
 router.route('/users')
     .get(function (req, res) {
-        res.render('users', {
-            title: "查看页面",
-            month: "Nov",
-            savings: "10000"
+        if (!("page" in req.query) || req.query.page <= 0) {
+            req.query.page = 1;
+        }
+
+        request('http://127.0.0.1:3000/api/users?page=' + req.query.page, function (err, response, body) {
+            if (err) {
+                console.log(err);
+                constantHelper(res, constHelper.paramError);
+                return;
+            }
+
+            res.render('users', {
+                title: "查看页面",
+                users: JSON.parse(body).data,
+                page:req.query.page+1
+            });
         })
-        ;
-        /* $.ajax({
-         type: "GET",
-         dataType: "json",
-         url: "../api/users?page=1",
-         //data: { id: id, name: name },
-         success: function(json) {
-         var typeData = json.Module;
-         $.each(typeData, function(i, n) {
-         var tbBody = ""
-         var trColor;
-         if (i % 2 == 0) {
-         trColor = "even";
-         }
-         else {
-         trColor = "odd";
-         }
-         tbBody += "<tr class='" + trColor + "'><td>" + n.ModuleNum + "</td>" + "<td>" + n.ModuleName + "</td>" + "<td>" + n.ModuleDes + "</td></tr>";
-         $("#myTb").append(tbBody);
-         });
-         },
-         error: function(json) {
-         alert("加载失败");
-         }
-         });
-         };*/
+
     });
 
 
